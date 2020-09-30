d3e3a10c94a0c6a541f6974e775f0f8fcf01a905
saurabh1498 <48017668+saurabh1498@users.noreply.github.com>
2020-09-30 21:08:29 +0530

    Update urls.py

--- websites/urls.py
+++ websites/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', include('traveller.urls')),
+    path('traveller/', include('traveller.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls'))
     
